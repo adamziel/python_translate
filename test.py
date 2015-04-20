12c41b99acecfcd394142c2435edb8eb45de6fd6
azielinski <kontakt@azielinski.info>
2015-04-20 12:48:29 +0200

    Test.py will now return a proper exit code if test failed

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
+import sys
 import unittest
 
 if __name__ == "__main__":
     suite = unittest.TestLoader().discover('./python_translate/tests', pattern = "*.py")
-    unittest.TextTestRunner(verbosity=2).run(suite)
-    
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    sys.exit(not result.wasSuccessful())
